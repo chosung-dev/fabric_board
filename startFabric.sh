5a15a48da5e3c97c15448533504cdb1bb55d9048
Kim-DongHyeon <40589716+Kim-DongHyeon@users.noreply.github.com>
2019-05-16 18:30:19 +0900

    Add files via upload

--- startFabric.sh
+++ startFabric.sh
@@ -23,33 +23,15 @@ cd fabric-samples/basic-network
 # Now launch the CLI container in order to install, instantiate chaincode
 # and prime the ledger with our 10 cars
 #docker-compose -f ./docker-compose.yml up -d cli
-#docker ps -a
+docker ps -a
 
-export PEER0_ORG1_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-export PEER0_ORG3_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
-
-# chaincode install
-echo "chaincode install in the peer0.org1.example.com"
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
-           -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
-           cli peer chaincode install -n fabric_board -v 1.0 -p "$CC_SRC_PATH"
-echo "chaincode install in the peer0.org3.example.com"
-docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" \
-           -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp" \
-           -e "CORE_PEER_CA_ROOTCERT_FILE=$PEER0_ORG3_CA" \
-           -e "CORE_PEER_ADDRESS=peer0.org3.example.com:7051" \
-           cli peer chaincode install -n fabric_board -v 1.0 -p "$CC_SRC_PATH"
-
-#chaincode instantiate
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
-           -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
-           cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabric_board -v 1.0 -c '{"Args":[]}' -P "OR('Org1MSP.member', 'Org3MSP.member')" 
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli_org1 peer chaincode install -n fabric_board -v 1.0 -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
+docker exec -e "PEER0_ORG3_CA=/opt/gopath/src/gihub.com/hyperledger/fabiric/peer/crypto/peerOrganizations/org3.example.com/peer/peer0.org3.example.com/tls/ca.crt" cli_org3 peer chaincode install -n fabric_board -v 1.0 -p "$CC_SRC_PATH" -l "$CC_RUNTIME_LANGUAGE"
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli_org1 peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n fabric_board -l "$CC_RUNTIME_LANGUAGE" -v 1.0 -c '{"Args":[]}' -P "OR ('Org1MSP.member','Org3MSP.member')"
 sleep 10
 
-# call initLedger
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" \
-           -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" \
-           cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabric_board -c '{"function":"initLedger","Args":[]}'
+
+#docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n fabric_board -c '{"function":"initLedger","Args":[]}'
 
 cat <<EOF
 
